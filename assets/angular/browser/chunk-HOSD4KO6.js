2c793e0bed71608df6429d15cb1cc7ab5ba17e38
Luis <cristian.98oruro@gmail.com>
2024-11-20 19:49:37 -0400

    optimizacion de recursos de carga mas disenio bellas artes

--- assets/angular/browser/chunk-HOSD4KO6.js
+++
@@ -1,1 +0,0 @@
-import{Db as e,Eb as t,Ob as s,Ta as m,n as r,s as i}from"./chunk-5EWIWO3F.js";var f=(()=>{class o{static \u0275fac=function(d){return new(d||o)};static \u0275mod=i({type:o});static \u0275inj=r({imports:[e,t,m,e,t,s]})}return o})();export{f as a};
